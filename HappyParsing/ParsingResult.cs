42f68197027a2a441bbdadad1534061c5a69f68a
emanuelefirmani <emanuelefirmani@kpmg.com>
2024-04-13 19:11:49 +0200

    Removal of unnecessary abstract methods

---
+++ HappyParsing/ParsingResult.cs
@@ -0,0 +1,37 @@
+﻿namespace HappyParsing;
+
+internal abstract record ParsingResult<T>
+{
+    public abstract T Value { get; }
+    public abstract string Message { get; }
+    public abstract TOut Match<TOut>(Func<T, TOut> whenSuccessful, Func<string, TOut> whenFailed);
+    
+    public record Success(T SuccessfulValue) : ParsingResult<T>
+    {
+        public override T Value => SuccessfulValue;
+        public override string Message => "";
+        public override TOut Match<TOut>(Func<T, TOut> whenSuccessful, Func<string, TOut> whenFailed) =>
+            whenSuccessful(Value);
+    }
+
+    public record Failure(string FailureMessage) : ParsingResult<T>
+    {
+        public override T Value => throw new Exception("Cannot access Value because in Failure");
+        public override string Message => FailureMessage;
+        public override TOut Match<TOut>(Func<T, TOut> whenSuccessful, Func<string, TOut> whenFailed) =>
+            whenFailed(Message);
+    }
+}
+
+internal static class ParsingResultExtensions
+{
+    internal static string GetMessage<T>(this IEnumerable<ParsingResult<T>> results) =>
+        string.Join("|",
+            results.Where(x => x.GetType() == typeof(ParsingResult<T>.Failure)).Select(x => x.Message));
+
+    internal static T? MatchValue<T>(this ParsingResult<T> result) =>
+        result.Match(
+            v => v,
+            _ => default(T?)
+        );
+}
--- HappyParsing/SimpleParser.cs
+++ HappyParsing/SimpleParser.cs
@@ -2,7 +2,7 @@
 
 internal class SimpleParser
 {
-    internal static DecimalDataObject ParseDecimal(InputDataObject value)
+    internal static DecimalDataObject ParseDecimalOld(InputDataObject value)
     {
         var (amount, returnedMessageAmount) = ParseOptionalDecimal(value.Amount, "Amount");
         var (size, returnedMessageSize) = ParseOptionalDecimal(value.Size, "Size");
@@ -17,10 +17,34 @@ internal class SimpleParser
         );
     }
 
+    internal static DecimalDataObject ParseDecimal(InputDataObject value)
+    {
+        var amount = ParseOptionalDecimalRecord(value.Amount, "Amount");
+        var size = ParseOptionalDecimalRecord(value.Size, "Size");
+        
+        var message = new [] { amount, size }.GetMessage();
+        
+        return new DecimalDataObject(
+            amount.MatchValue(),
+            size.MatchValue(),
+            amount.MatchValue() * size.MatchValue(),
+            message
+        );
+    }
+
     private static (decimal?, string) ParseOptionalDecimal(string value, string fieldName) =>
         !string.IsNullOrEmpty(value)
             ? decimal.TryParse(value, out var parsedAmount)
                 ? (parsedAmount, "")
                 : (null, $"invalid {fieldName}")
             : (null, "");
+
+    private static ParsingResult<decimal?> ParseOptionalDecimalRecord(string value, string fieldName) =>
+        !string.IsNullOrEmpty(value)
+            ? decimal.TryParse(value, out var parsedAmount)
+                ? new ParsingResult<decimal?>.Success(parsedAmount)
+                : new ParsingResult<decimal?>.Failure($"invalid {fieldName}")
+            : new ParsingResult<decimal?>.Success(null);
+    
+    
 }
